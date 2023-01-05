a721d925be731e71fa9d0453755807111db31796
SigitOkta <oktasigit@gmail.com>
2023-01-05 12:20:34 +0700

    add detail Layout

---
+++ app/src/main/java/com/dwarfkit/storilia/pkg/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dwarfkit.storilia.pkg.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dwarfkit.storilia.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
